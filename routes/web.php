1fb7ae56b58172845b4ff39648e7ea1c248ce2c8
Azizul109 <mdazizularif109@gmail.com>
2022-04-14 23:05:43 +0600

    controller series completed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,20 +17,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/contact', function () {
-    return "this is contact page";
-});
+//Route::get('/contact', function () {
+//    return "this is contact page";
+//});
+//
+//Route::get('/about', function () {
+//    return "this is about page";
+//});
+//
+//Route::get('/admin/posts/{id}/{name}', function ($id, $name) {
+//    return "this is post number " . $id . " " . $name;
+//});
+//
+//Route::get('/admin/posts/example', array('as'=>'admin.home', function(){
+//    $url = route('admin.home');
+//
+//    return "this url is ". $url;
+//}));
 
-Route::get('/about', function () {
-    return "this is about page";
-});
+//Route::get('/post/{id}', [PostsController::class, 'index']);
 
-Route::get('/admin/posts/{id}/{name}', function ($id, $name) {
-    return "this is post number " . $id . " " . $name;
-});
-
-Route::get('/admin/posts/example', array('as'=>'admin.home', function(){
-    $url = route('admin.home');
-
-    return "this url is ". $url;
-}));
+Route::resource('posts', PostsController::class);
