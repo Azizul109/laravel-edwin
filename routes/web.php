dff8ffe44ab7465db556089ac47495d563a45207
Azizul109 <mdazizularif109@gmail.com>
2022-04-15 12:07:10 +0600

    raw sql queries series

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostsController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -37,8 +39,36 @@ Route::get('/', function () {
 
 //Route::get('/post/{id}', [PostsController::class, 'index']);
 
-Route::resource('posts', PostsController::class);
+//Route::resource('posts', PostsController::class);
+//
+//Route::get('/contact', [PostsController::class, 'contactView']);
+//
+//Route::get('/post/{id}/{name}/{age}', [PostsController::class, 'showPost']);
 
-Route::get('/contact', [PostsController::class, 'contactView']);
 
-Route::get('/post/{id}/{name}/{age}', [PostsController::class, 'showPost']);
+//Database raw sql queries
+
+Route::get('/insert', function () {
+    DB::insert('insert into posts(title, body) values(?, ?)', ['PHP with laravel', 'PHP laravel the best thing happen to php']);
+});
+
+Route::get('/read', function () {
+    $results = DB::select('select * from posts where id = ?', [1]);
+
+    foreach ($results as $result) {
+
+        return $result->title;
+    }
+});
+
+Route::get('/update', function () {
+    $updated = DB::update('update posts set title = "Update title" where id = ?', [1]);
+
+    return $updated;
+});
+
+Route::get('/delete', function () {
+    $deleted = DB::delete('delete from posts where id = ?', [1]);
+
+    return $deleted;
+});
